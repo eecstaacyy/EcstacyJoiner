02d15d9754611228d4e310e2c265e22b45a60e1d
eecstaacyy <mem0ryreboot@icloud.com>
2024-05-26 12:17:16 +1000

    Update PlayerListener.java

--- me/ecstacy/ecstacyjoiner/PlayerListener.java
+++ me/ecstacy/ecstacyjoiner/PlayerListener.java
@@ -2,9 +2,6 @@ package me.ecstacy.ecstacyjoiner;
 
 import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.permission.Permission;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
@@ -18,7 +15,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class PlayerListener implements Listener, CommandExecutor {
+public class PlayerListener implements Listener {
     private Main plugin;
     private Map<String, String> playerJoiners = new HashMap<>();
     private Permission perms = null;
@@ -32,14 +29,13 @@ public class PlayerListener implements Listener, CommandExecutor {
         if (!this.playerDataFile.exists()) {
             try {
                 this.playerDataFile.createNewFile();
+                plugin.getLogger().info("playerData.yml file created.");
             } catch (IOException e) {
-                e.printStackTrace();
+                plugin.getLogger().severe("Could not create playerData.yml file: " + e.getMessage());
             }
         }
         this.playerData = YamlConfiguration.loadConfiguration(this.playerDataFile);
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
-        plugin.getCommand("ejoiner").setExecutor(this);
-        plugin.getCommand("ecstacyjoiner").setExecutor(this);
         setupPermissions();
         setupChat();
         plugin.saveDefaultConfig();
@@ -59,73 +55,33 @@ public class PlayerListener implements Listener, CommandExecutor {
         }
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(Main.colorize("&cThis command can only be used by players."));
-            return true;
-        }
-        Player player = (Player) sender;
-        FileConfiguration config = plugin.getConfig();
-
-        if (args.length == 0 || (args.length == 1 && args[0].equalsIgnoreCase("set"))) {
-            String helpMessage = config.getString("joiner_help_message");
-            if (helpMessage != null) {
-                player.sendMessage(Main.colorize(helpMessage));
-            }
-            return true;
-        } else if (args.length == 2 && args[0].equalsIgnoreCase("set")) {
-            String joinerId = args[1];
-            if (config.contains("joiners." + joinerId)) {
-                String permission = config.getString("joiners." + joinerId + ".permission");
-                if (permission != null && perms != null && !perms.has(player, permission)) {
-                    String noPermissionMessage = config.getString("no_permission");
-                    if (noPermissionMessage != null) {
-                        player.sendMessage(Main.colorize(noPermissionMessage));
-                    }
-                    return true;
-                }
-                playerJoiners.put(player.getName(), joinerId);
-                playerData.set(player.getUniqueId().toString(), joinerId);
-                savePlayerData();
-                String joinerSuccessfullySetMessage = config.getString("joiner_successfully_set");
-                if (joinerSuccessfullySetMessage != null) {
-                    player.sendMessage(Main.colorize(joinerSuccessfullySetMessage));
-                }
-            } else {
-                String joinerNotFoundMessage = config.getString("joiner_not_found");
-                if (joinerNotFoundMessage != null) {
-                    player.sendMessage(Main.colorize(joinerNotFoundMessage));
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         FileConfiguration config = plugin.getConfig();
 
         String joinerId = playerData.getString(player.getUniqueId().toString(), null);
-        if (joinerId == null || !config.contains("joiners." + joinerId)) {
+        
+        if (joinerId != null && (!config.contains("joiners." + joinerId) || !perms.has(player, config.getString("joiners." + joinerId + ".permission")))) {
             joinerId = null;
+        }
+
+        if (joinerId == null) {
             for (String key : config.getConfigurationSection("joiners").getKeys(false)) {
                 String permission = config.getString("joiners." + key + ".permission");
-                if (permission == null || (perms != null && perms.has(player, permission))) {
+                if (permission == null || perms.has(player, permission)) {
                     joinerId = key;
+                    break;
                 }
             }
-            if (joinerId != null) {
-                playerJoiners.put(player.getName(), joinerId);
-                playerData.set(player.getUniqueId().toString(), joinerId);
-                savePlayerData();
-            } else {
-                playerJoiners.remove(player.getName());
-            }
-        } else {
+        }
+
+        if (joinerId != null) {
             playerJoiners.put(player.getName(), joinerId);
+            playerData.set(player.getUniqueId().toString(), joinerId);
+            savePlayerData();
+        } else {
+            playerJoiners.remove(player.getName());
         }
 
         String message = joinerId != null ? config.getString("joiners." + joinerId + ".message") : null;
@@ -144,11 +100,15 @@ public class PlayerListener implements Listener, CommandExecutor {
         }
     }
 
-    private void savePlayerData() {
+    public FileConfiguration getPlayerData() {
+        return playerData;
+    }
+
+    public void savePlayerData() {
         try {
             playerData.save(playerDataFile);
         } catch (IOException e) {
-            e.printStackTrace();
+            plugin.getLogger().severe("Could not save playerData.yml file: " + e.getMessage());
         }
     }
 }
