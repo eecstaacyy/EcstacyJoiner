dff9340af3288d4563530fbb054c768ff6e490b1
eecstaacyy <mem0ryreboot@icloud.com>
2024-05-26 12:17:00 +1000

    Update Main.java

--- me/ecstacy/ecstacyjoiner/Main.java
+++ me/ecstacy/ecstacyjoiner/Main.java
@@ -4,15 +4,23 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+    private PlayerListener playerListener;
+
     @Override
     public void onEnable() {
-        new PlayerListener(this);
+        this.playerListener = new PlayerListener(this);
+        PlayerCommandHandler commandHandler = new PlayerCommandHandler(this);
+        getCommand("ejoiner").setExecutor(commandHandler);
         getLogger().info("EcstacyJoiner has been enabled!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("EcstacyJoiner has been disabled.");
+    }
+
+    public PlayerListener getPlayerListener() {
+        return playerListener;
     }
 
     public static String colorize(String message) {
